28ce8f098b07dbcc9e68f9f0f0b66ebcec8545ce
MBOUDOU <adalbertmboudou100@gmail.com>
2025-11-18 00:44:59 +0100

    Intégration du front-end (index.html, style.css, script.js) et ajout du HomeController pour la redirection vers la page d’accueil

---
+++ pageaccueil/src/test/java/com/example/pageaccueil/PageaccueilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pageaccueil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageaccueilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
