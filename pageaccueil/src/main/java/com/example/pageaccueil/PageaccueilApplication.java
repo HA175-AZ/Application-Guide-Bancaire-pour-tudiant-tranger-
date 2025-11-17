a87bf305bc9cd62ce747beb4cf0276b579a915e8
MBOUDOU <adalbertmboudou100@gmail.com>
2025-11-18 00:54:47 +0100

    fichier indispensable pour le lancement de l'application Spring Boot.

---
+++ pageaccueil/src/main/java/com/example/pageaccueil/PageaccueilApplication.java
@@ -0,0 +1,12 @@
+package com.example.pageaccueil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageaccueilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageaccueilApplication.class, args);
+    }
+}
